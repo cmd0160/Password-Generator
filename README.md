291a86f54ec3b49b9441faeb4e723a1f6840395a
cdtest <corydavis1989@gmail.com>
2022-03-06 17:47:49 -0600

    added readme with assignment specifications

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+This application assists you in easliy creating a password that consists of user selected groups of characters and a user inputted ength. To operated the password generator, navigate to the deployed site and follow the window confirmations and prompts. The first confirm is for uppercase characters. Select 'OK' to confirm uppercase character usage and cancel to not include them. You will then follow the same process for lowercase, numbers and special characters. The final prompt asks for the character length. Enter a number between 8 and 128 and the application will then provide you with a unique password using the provided specifications.
+
+## Link to GitHub Repository
+https://github.com/cmd0160/Password-Generator
+
+## Link to Deployed Site
+https://cmd0160.github.io/Password-Generator/
+
+## Application Home Page
+![Password Generator Home Page](assets/images/Password-Generator.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,71 @@
-// Assignment code here
-const upperChars = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-const lowerChars = ['abcdefghijklmnopqrstuvwxyz'];
-const numChars = ['0123456789'];
-const specChars = ['!@#$%^&*()_+=-'];
+const upperChars = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const lowerChars = ["abcdefghijklmnopqrstuvwxyz"];
+const numChars = ['1234567890'];
+const specChars = ["!@#$%^&*()_+=-"];
 let passwordArr = [];
 let newPass = [];
 
-
-
-
 function generatePassword() {
-  newPass = [];
   var upper = window.confirm("Do you want to include Uppercase Characters?");
   if (upper) {
-    passwordArr = passwordArr + upperChars;
+    passwordArr += upperChars;
     alert("You are inluding uppercase characters");
   } else {
     alert("You are not including uppercase characters");
   }
-  
+
   var lower = window.confirm("Do you want to include Lower Characters?");
   if (lower) {
-    passwordArr = passwordArr + lowerChars;
+    passwordArr += lowerChars;
     alert("You are inluding Lower characters");
   } else {
     alert("You are not including Lower characters");
   }
-  
+
   var nums = window.confirm("Do you want to include Number Characters?");
   if (nums) {
-    passwordArr = passwordArr + numChars;
+    passwordArr += numChars;
     alert("You are inluding Number characters");
   } else {
     alert("You are not including Number characters");
   }
-  
+
   var specs = window.confirm("Do you want to include Special Characters?");
   if (specs) {
-    passwordArr = passwordArr + specChars;
+    passwordArr += specChars;
     alert("You are inluding Special characters");
   } else {
     alert("You are not including Special characters");
   }
-  // console.log(passwordArr);
-  
+
   var passwordLength = parseInt(
     window.prompt(
       "How many characters would you like to include in your password? Please select a number between 8 and 128."
     )
   );
-  alert(`You have chosen to include ${passwordLength} characters in your password.`);
+  alert(
+    `You have chosen to include ${passwordLength} characters in your password.`
+  );
   if (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Please pick a number between 8 and 128.");
   }
-  // console.log(passwordLength);
 
-
-  for(i = 1; i <= passwordLength; i++) {
-    let randPasswordChar =  Math.floor(Math.random() * passwordArr.length + 1);
+  for (i = 1; i <= passwordLength; i++) {
+    let randPasswordChar = Math.floor(Math.random() * passwordArr.length);
     let oneChar = passwordArr[randPasswordChar];
     newPass += oneChar;
   }
 
-  return newPass ;
+  return newPass;
 }
-
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
+  passwordArr = [];
+  newPass = [];
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -84,5 +74,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
