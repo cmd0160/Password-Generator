362d07e5a19fad309393951eedf8cdaec47a37b0
cdtest <corydavis1989@gmail.com>
2022-03-06 16:00:21 -0600

    fixed comma issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-const upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-const lowerChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const numChars = [0,1,2,3,4,5,6,7,8,9];
-const specChars = ['!','@','#','$','%','^','&','*','(',')','_','+'];
+const upperChars = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const lowerChars = ['abcdefghijklmnopqrstuvwxyz'];
+const numChars = ['0123456789'];
+const specChars = ['!@#$%^&*()_+=-'];
 let passwordArr = [];
 let newPass = [];
 
